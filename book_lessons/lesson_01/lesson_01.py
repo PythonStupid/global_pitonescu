85bf425608b8a3356f4bc003e9205b947d76064c
Moe Green <gearmobile@gmail.com>
2023-11-05 22:08:43 +0300

    init project

---
+++ book_lessons/lesson_01/lesson_01.py
@@ -0,0 +1,11 @@
+# name = 'ada lovelace'
+# print(name.capitalize())
+# print(name.title())
+
+# name_uppercase = name.upper()
+# print(name_uppercase.lower())
+
+first_name = 'ada'
+last_name = 'lovelace'
+full_name = f'{first_name} {last_name}'
+print(f'First programmer is {full_name.title()}')
