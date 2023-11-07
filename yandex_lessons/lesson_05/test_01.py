3906e8154e883db061d8230b5a37b43a71cbb5f1
Moe Green <gearmobile@gmail.com>
2023-11-07 12:08:09 +0300

    add if-else

---
+++ yandex_lessons/lesson_05/test_01.py
@@ -0,0 +1,6 @@
+apples_count = 20
+
+if apples_count > 10:
+    print('У вас больше 10 яблок!')
+else:
+    print('У вас меньше 10 яблок!')
---
+++ yandex_lessons/lesson_05/test_06.py
@@ -0,0 +1,10 @@
+beaufort = 7
+
+if beaufort == 0:
+    print('Штиль')
+elif beaufort == 1:
+    print('Тихий ветер')
+elif beaufort == 5:
+    print('Свежий ветер')
+else:
+    print('Шторм')
