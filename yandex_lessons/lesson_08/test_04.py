110e787e6cd5927fd68854fef9726c6ee123a011
Moe Green <gearmobile@gmail.com>
2023-11-08 22:01:30 +0300

    count

---
+++ yandex_lessons/lesson_08/test_04.py
@@ -0,0 +1,13 @@
+def number_of_occurrences(char, string):
+    count = 0
+
+    for letter in string:
+        if letter == char:
+            count += 1
+
+    print('Исходная строка:', string)
+    print('Количество вхождений символа', char, 'составляет:', count)
+
+phrase = 'Не волнуйтесь, если что-то не работает. Если бы всё работало, вас бы уволили.'
+
+number_of_occurrences('е', phrase)
