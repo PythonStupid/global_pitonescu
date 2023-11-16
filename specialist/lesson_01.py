795f21e2898f0e30ac2ef71faa2130a906168aee
Moe Green <gearmobile@gmail.com>
2023-11-16 23:06:23 +0300

    next step

---
+++ specialist/lesson_01.py
@@ -0,0 +1,15 @@
+# message = 'Enter your name '
+# greeting = 'Hello,'
+
+# user_name = input(message)
+
+# print(f'{greeting} {user_name}')
+
+n1 = 2
+n2 = 3
+
+message = f'{n1} + {n2} = {n1 + n2}'
+print(message)
+
+path = r'c:\path1\path2\path3'
+print(path)
